ed022e97bbad7bfeddee0b1e9a59a30b0ac3d15a
Claudiu <lzr_tg@yahoo.com>
2018-04-10 12:09:34 +0300

    Adaugat galerie.
    https://github.com/liwenyip/hugo-easy-gallery

--- themes/hugo-casper-two/static/js/custom.js
+++ themes/hugo-casper-two/static/js/custom.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    // Limitare text din casute pe baza de randuri, cu ellipsis la final
     var modules = document.getElementsByClassName("js-text-clamp");
 
     $.each(modules, function (index, module) {
