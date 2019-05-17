6899b0e227666421c1d0f2fb9438fa92eea306e3
cristan <13849765+cristan2@users.noreply.github.com>
2019-05-17 12:35:02 +0100

    General: clamp fix, acum se scurteaza textul si de pe primul card din pagina, cu exceptia celui de pe home page

--- themes/hugo-casper-two/static/js/custom.js
+++ themes/hugo-casper-two/static/js/custom.js
@@ -3,8 +3,6 @@ $(document).ready(function () {
     var modules = document.getElementsByClassName("js-text-clamp");
 
     $.each(modules, function (index, module) {
-        if(index > 0){
-            $clamp(module, {clamp: 6});
-        }
+        $clamp(module, {clamp: 6});
     });
 });
